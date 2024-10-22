f3370eec0de2f64989a8654727bec4ef6f9ea998
Alex Zarankin <w0618362@myscc.ca>
2024-10-22 14:40:55 -0400

    Created database class

---
+++ src/main/java/com/example/daycaremanagement/database/Const.java
@@ -0,0 +1,9 @@
+package com.example.daycaremanagement.database;
+
+public class Const {
+    // take this out later, read the login info from a file
+    // temporary
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASS = "";
+}
