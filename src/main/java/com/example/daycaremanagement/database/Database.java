33d09cdae7487cca3ac9c46e3fd97c1b09095771
Ben MacIntyre <W0840839@myscc.ca>
2024-10-23 15:26:06 -0400

    Change Database Exception Handling * Database constructor now thros exceptions instead of catching * Database getInstance now throws exception instead of catching * This will help create custom error messages in mainApp for user

--- src/main/java/com/example/daycaremanagement/database/Database.java
+++ src/main/java/com/example/daycaremanagement/database/Database.java
@@ -9,18 +9,14 @@ public class Database {
     private static Database instance;
     private Connection connection;
 
-    private Database(){
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/" + DB_NAME +"?serverTimezone=UTC", DB_USER, DB_PASS);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+    private Database() throws Exception{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/" + DB_NAME +"?serverTimezone=UTC", DB_USER, DB_PASS);
     }
 
     public Connection getConnection() {return connection;}
 
-    public static Database getInstance(){
+    public static Database getInstance() throws Exception{
         if (instance == null){
             instance = new Database();
         }
