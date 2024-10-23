c273441186f5d7c64a0cadac5d449743c9b68a01
Ben MacIntyre <W0840839@myscc.ca>
2024-10-23 15:52:20 -0400

    #9 Adds connectToDatabase * Cleans up code a bit * throws all the connection logic into one function for reuse

--- src/main/java/com/example/daycaremanagement/MainApp.java
+++ src/main/java/com/example/daycaremanagement/MainApp.java
@@ -21,62 +21,37 @@ import static com.example.daycaremanagement.AppConst.SCREEN_WIDTH;
 
 public class MainApp extends Application {
     public static Stage primaryStage;
+    private LoginPageScene loginPage = new LoginPageScene();
+    // Test Page for login
+    private BorderPane testRoot = new BorderPane();
+    private Scene testScene = new Scene(testRoot, SCREEN_WIDTH, SCREEN_HEIGHT);
+
 
     @Override
     public void start(Stage stage) throws IOException {
         // Initalization
-        LoginPageScene loginPage = new LoginPageScene();
         primaryStage = stage;
         primaryStage.setTitle("Daycare Management");
-
-
-        // Test Page for login
-        BorderPane testRoot = new BorderPane();
-        Scene testScene = new Scene(testRoot, SCREEN_WIDTH, SCREEN_HEIGHT);
 
         // Set the Logic for login button press in LoginPageScene
         // Has to grab the button in main app for the ability to change the primary stage to test page
         LoginPagePane loginpagepane = (LoginPagePane)(loginPage.getRoot());
         loginpagepane.getLoginButton().setOnAction(e -> {
             if(loginpagepane.saveLoginInfo(loginpagepane.getDbNameInput(), loginpagepane.getUsernameInput(), loginpagepane.getHiddenPassInput())) {
-                // Set Consts Here
-                if (setConst()) {
-                    // Check Connection here
-                    if (isConnected()) {
-                        primaryStage.setScene(loginPage);
-                    } else {
-                        loginpagepane.getMessageLabel().setText("Error Connecting to Database");
-                    }
-                } else {
-                    loginpagepane.getMessageLabel().setText("Error Setting Constants");
-                }
+                connectToDatabase();
             }
         });
 
         // Login Page Logic
         if (loginExists()) {
-            // Set Consts Here
-            if (setConst()) {
-                // Check Connection here
-                if (isConnected()) {
-                    primaryStage.setScene(loginPage);
-                } else {
-                    loginpagepane.getMessageLabel().setText("Error Connecting to Database");
-                }
-            } else {
-                loginpagepane.getMessageLabel().setText("Error Setting Constants");
-            }
-
-            primaryStage.setScene(testScene);
+            connectToDatabase();
         } else {
             primaryStage.setScene(loginPage);
         }
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) {launch();}
 
     /**
      * Check if the "login/const.txt" file exists to decide if login is needed.
@@ -143,6 +118,26 @@ public class MainApp extends Application {
             // If the database connection fails, display an error and stay on the login screen
             // Returns false
             return false;
+        }
+    }
+
+
+    /**
+     * Checks connection and sets scene
+     * Cleans up code by putting the logic for a connection in one function
+     * */
+    private void connectToDatabase() {
+        LoginPagePane loginpagepane = (LoginPagePane)(loginPage.getRoot());
+        // Set Consts Here
+        if (setConst()) {
+            // Check Connection here
+            if (isConnected()) {
+                primaryStage.setScene(testScene);
+            } else {
+                loginpagepane.getMessageLabel().setText("Error Connecting to Database");
+            }
+        } else {
+            loginpagepane.getMessageLabel().setText("Error Setting Constants");
         }
     }
 }
