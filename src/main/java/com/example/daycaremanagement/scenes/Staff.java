88f3c8c45939e4fdd157d53a3a6e0b842b7492ec
Ben MacIntyre <W0840839@myscc.ca>
2024-11-10 23:59:04 -0500

    Implements sidebar to Students * Added side bar to students page * Added content section with title

--- src/main/java/com/example/daycaremanagement/scenes/Staff.java
+++ src/main/java/com/example/daycaremanagement/scenes/Staff.java
@@ -1,29 +1,53 @@
 package com.example.daycaremanagement.scenes;
 
+import com.example.daycaremanagement.interfaces.SideBar;
 import javafx.scene.layout.BorderPane;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-public class Staff extends BorderPane {
-  public Staff() {
-    // Create the left sidebar
-    VBox leftSidebar = new VBox();
-    leftSidebar.setPadding(new Insets(10));
-    leftSidebar.setSpacing(8);
+public class Staff extends BorderPane implements SideBar {
+  private static Staff instance;
+  private BorderPane content = new BorderPane();
 
-    // Add components to the sidebar
-    Label titleLabel = new Label("Staff Management");
-    Button addButton = new Button("Add Staff");
-    Button editButton = new Button("Edit Staff");
-    Button deleteButton = new Button("Delete Staff");
+  /**
+   * Gets an instance of this class
+   * @return the instance
+   */
+  public static Staff getInstance(){
+    if (instance == null){
+      instance = new Staff();
+    }
+    return instance;
+  }
+  private Staff() {
+    this.setLeft(sideBar());
+    this.setBottom(bottomBar());
+    content.setTop(new Label("Students"));
+    this.setCenter(content);
+  }
 
-    // Add components to the VBox
-    leftSidebar.getChildren().addAll(titleLabel, addButton, editButton, deleteButton);
 
-    // Set the left sidebar in the BorderPane
-    this.setLeft(leftSidebar);
+  @Override
+  public VBox sideBar() {
+    return SideBar.super.sideBar();
+  }
+
+  @Override
+  public HBox bottomBar() {
+    return SideBar.super.bottomBar();
+  }
+
+  @Override
+  public void sideButtonBar() {
+
+  }
+
+  @Override
+  public void bottomButtonBar() {
+
   }
 }
 
