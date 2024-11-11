a947d46dfc6fdcca5792aef72d8dee69b3105722
Ben MacIntyre <W0840839@myscc.ca>
2024-11-10 23:57:22 -0500

    Implements sidebar to Students * Added sidebar to students * Added cantent section with title label

--- src/main/java/com/example/daycaremanagement/scenes/Students.java
+++ src/main/java/com/example/daycaremanagement/scenes/Students.java
@@ -1,6 +1,50 @@
 package com.example.daycaremanagement.scenes;
 
+import com.example.daycaremanagement.interfaces.SideBar;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class Students extends BorderPane {
+public class Students extends BorderPane implements SideBar {
+    private static Students instance;
+    private BorderPane content = new BorderPane();
+
+    /**
+     * Gets an instance of this class
+     * @return the instance
+     */
+    public static Students getInstance(){
+        if (instance == null){
+            instance = new Students();
+        }
+        return instance;
+    }
+    private Students() {
+        this.setLeft(sideBar());
+        this.setBottom(bottomBar());
+        content.setTop(new Label("Students"));
+        this.setCenter(content);
+    }
+
+
+    @Override
+    public VBox sideBar() {
+        return SideBar.super.sideBar();
+    }
+
+    @Override
+    public HBox bottomBar() {
+        return SideBar.super.bottomBar();
+    }
+
+    @Override
+    public void sideButtonBar() {
+
+    }
+
+    @Override
+    public void bottomButtonBar() {
+
+    }
 }
