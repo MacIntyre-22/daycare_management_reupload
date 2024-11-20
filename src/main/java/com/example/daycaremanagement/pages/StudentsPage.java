8e79e4fcf9484aeb64c04ec2781174f302c733a7
Ben MacIntyre <W0840839@myscc.ca>
2024-11-20 13:47:59 -0500

    Refactored Setting data for barchart * I made a function to get the data by room * This cleans up the code a bit and adds the data properly

--- src/main/java/com/example/daycaremanagement/pages/StudentsPage.java
+++ src/main/java/com/example/daycaremanagement/pages/StudentsPage.java
@@ -110,48 +110,42 @@ public class StudentsPage extends CrudOverlay {
             barChart.setTitle("Age Count");
 
             // Series
+            ArrayList<XYChart.Series<String, Number>> seriesArray = new ArrayList<>();
             // Each Room is a series
-            String series1Name = "<1";
-            XYChart.Series<String, Number> age1 = new XYChart.Series<>();
-            age1.setName(series1Name);
+            String series0Name = "<1";
+            XYChart.Series<String, Number> age0 = new XYChart.Series<>();
+            age0.setName(series0Name);
+            seriesArray.add(age0);
             // Set Data for all series for each room under here
 
-            String series2Name = "1";
+            String series1Name = "1";
+            XYChart.Series<String, Number> age1 = new XYChart.Series<>();
+            age1.setName(series1Name);
+            seriesArray.add(age1);
+
+            String series2Name = "2";
             XYChart.Series<String, Number> age2 = new XYChart.Series<>();
             age2.setName(series2Name);
+            seriesArray.add(age2);
 
-            String series3Name = "2";
+            String series3Name = "3";
             XYChart.Series<String, Number> age3 = new XYChart.Series<>();
             age3.setName(series3Name);
+            seriesArray.add(age3);
 
-            String series4Name = "3";
+            String series4Name = "4";
             XYChart.Series<String, Number> age4 = new XYChart.Series<>();
             age4.setName(series4Name);
+            seriesArray.add(age4);
 
-            String series5Name = "4";
-            XYChart.Series<String, Number> age5 = new XYChart.Series<>();
-            age5.setName(series5Name);
+            // Add data using setDataByRoom()
+            setDataByRoom(seriesArray, 1);
+            setDataByRoom(seriesArray, 2);
+            setDataByRoom(seriesArray, 3);
+            setDataByRoom(seriesArray, 4);
+            setDataByRoom(seriesArray, 5);
 
 
-            // Storing Data from students
-            for (Student student : students.getAllStudents()) {
-                if (student.getAge() < 1) {
-                    age1.getData().add(new XYChart.Data(getRoomName(student), student.getAge()));
-                } else {
-                    switch ((int) student.getAge()) {
-                        case 1:
-                            age2.getData().add(new XYChart.Data(getRoomName(student), student.getAge()));
-                        case 2:
-                            age3.getData().add(new XYChart.Data(getRoomName(student), student.getAge()));
-                        case 3:
-                            age4.getData().add(new XYChart.Data(getRoomName(student), student.getAge()));
-                        case 4:
-                            age5.getData().add(new XYChart.Data(getRoomName(student), student.getAge()));
-                    }
-                }
-            }
-
-            barChart.getData().addAll(age1, age2, age3, age4, age5);
             barChart.setLegendSide(Side.LEFT);
             content.setCenter(barChart);
 
@@ -250,5 +244,49 @@ public class StudentsPage extends CrudOverlay {
             default -> "";
         };
         return room;
+    }
+
+    /**
+     * Takes series for barchart and adds the data, from students table, based on a given room id.
+     * @param seriesArray
+     * @param roomId
+     */
+    private void setDataByRoom(ArrayList<XYChart.Series<String, Number>> seriesArray, int roomId) {
+        ArrayList<Student> students = this.students.getAllStudents();
+        // Counts for each age
+        int count0 = 0, count1 = 0, count2 = 0, count3 = 0, count4 = 0;
+        // Sorted students by room
+        ArrayList<Student> studentsList = new ArrayList<>();
+
+        // Popular studentList
+        for (Student student : students) {
+            if (student.getRoom_id() == roomId) {
+                studentsList.add(student);
+            }
+        }
+
+        // Get age count for room
+        for (Student student : studentsList) {
+            System.out.println(student.getAge());
+            switch ((int) student.getAge()) {
+                case 0:
+                    count0++;
+                case 1:
+                    count1++;
+                case 2:
+                    count2++;
+                case 3:
+                    count3++;
+                case 4:
+                    count4++;
+            }
+        }
+
+        // Add data to room
+        seriesArray.get(0).getData().add(new XYChart.Data(roomTable.getRoom(roomId).getName(), count0));
+        seriesArray.get(1).getData().add(new XYChart.Data(roomTable.getRoom(roomId).getName(), count1));
+        seriesArray.get(2).getData().add(new XYChart.Data(roomTable.getRoom(roomId).getName(), count2));
+        seriesArray.get(3).getData().add(new XYChart.Data(roomTable.getRoom(roomId).getName(), count3));
+        seriesArray.get(4).getData().add(new XYChart.Data(roomTable.getRoom(roomId).getName(), count4));
     }
 }
