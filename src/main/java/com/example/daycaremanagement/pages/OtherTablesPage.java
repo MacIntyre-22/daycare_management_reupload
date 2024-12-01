5756feb3dafa866bc7d3c79acd313ae6432fd49b
Ben MacIntyre <W0840839@myscc.ca>
2024-11-30 21:09:38 -0500

    Added Constructor * Added a constructor to OtherTablesPage * Added the first table as well

--- src/main/java/com/example/daycaremanagement/pages/OtherTablesPage.java
+++ src/main/java/com/example/daycaremanagement/pages/OtherTablesPage.java
@@ -1,16 +1,24 @@
 package com.example.daycaremanagement.pages;
 
 import com.example.daycaremanagement.overlays.CrudOverlay;
-import com.example.daycaremanagement.tables.CityTable;
-import com.example.daycaremanagement.tables.GuardianStudentRelationTable;
-import com.example.daycaremanagement.tables.PositionTable;
-import com.example.daycaremanagement.tables.RoomTable;
+import com.example.daycaremanagement.pojo.Guardian;
+import com.example.daycaremanagement.pojo.GuardianStudentRelation;
+import com.example.daycaremanagement.tables.*;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.sql.SQLException;
 
 public class OtherTablesPage extends CrudOverlay {
     // DF
     private static OtherTablesPage instance;
     private Label title = new Label("Other Tables");
+
+    // Active table
+    int active = 0;
+
     private PositionTable posTable;
     private RoomTable roomTable;
     private CityTable cityTable;
@@ -28,6 +36,19 @@ public class OtherTablesPage extends CrudOverlay {
         return instance;
     }
 
+    private OtherTablesPage() {
+        super();
+        content.setTop(title);
+
+        // Set Icons for buttons we use
+        graph1.setGraphic(createBtn(setIcon(ICONS[0], 30), "Rel."));
+        graph2.setGraphic(createBtn(setIcon(ICONS[1], 30), "Room"));
+        graph3.setGraphic(createBtn(setIcon(ICONS[2], 30), "Pos."));
+
+        loadTable();
+        loadInfo();
+    }
+
     @Override
     protected void sideButtonBar() {
 
@@ -40,6 +61,31 @@ public class OtherTablesPage extends CrudOverlay {
 
     @Override
     protected void loadTable() {
+        // Set active table to 0
+        active = 0;
+        title.setText("Single Parent - Student Relation");
 
+        TableView tableView = new TableView();
+        // Grab table data
+        try {
+            relationTable = GuardianStudentRelationTable.getInstance();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        // Create Columns
+        TableColumn<GuardianStudentRelation, String> column1 = new TableColumn<>("Parent Id");
+        column1.setCellValueFactory(e -> new SimpleStringProperty(String.valueOf(e.getValue().getGuardian_id())));
+
+        TableColumn<GuardianStudentRelation, String> column2 = new TableColumn<>("Student Id");
+        column2.setCellValueFactory(e -> new SimpleStringProperty(String.valueOf(e.getValue().getStudent_id())));
+
+
+        tableView.getColumns().addAll(column1, column2);
+        tableView.getItems().addAll(relationTable.getAllRelations());
+        tableView.setStyle("");
+
+        this.content.setCenter(tableView);
     }
 }
